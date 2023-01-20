8cac88ccbdfa5a711c20cc11d8a92c444eb47b74
Rowars2 <romain.parisot@edu.devinci.fr>
2023-01-20 15:38:38 +0100

    affiche le nom dans la page clavier si le numero existe dans contact

--- src/views/ClavierView.vue
+++ src/views/ClavierView.vue
@@ -3,19 +3,24 @@
         <h1>Clavier</h1>
         <div class="pad">
             <form @submit.prevent="call">
-                <button @click="addnumber(1)">1</button>
-                <button @click="addnumber(2)">2</button>
-                <button @click="addnumber(3)">3</button>
-                <button @click="addnumber(4)">4</button>
-                <button @click="addnumber(5)">5</button>
-                <button @click="addnumber(6)">6</button>
-                <button @click="addnumber(7)">7</button>
-                <button @click="addnumber(8)">8</button>
-                <button @click="addnumber(9)">9</button>
-                <button @click="addnumber(0)">0</button>
+                <button @click="addnumber(1), namecontact(currentnumber)">1</button>
+                <button @click="addnumber(3), namecontact(currentnumber)">3</button>
+                <button @click="addnumber(4), namecontact(currentnumber)">4</button>
+                <button @click="addnumber(2), namecontact(currentnumber)">2</button>
+                <button @click="addnumber(5), namecontact(currentnumber)">5</button>
+                <button @click="addnumber(6), namecontact(currentnumber)">6</button>
+                <button @click="addnumber(7), namecontact(currentnumber)">7</button>
+                <button @click="addnumber(8), namecontact(currentnumber)">8</button>
+                <button @click="addnumber(9), namecontact(currentnumber)">9</button>
+                <button @click="addnumber(0), namecontact(currentnumber)">0</button>
                 <button type="submit">Appeler</button>
             </form>
             <h2>numéro: {{ currentnumber }}</h2>
+            <div v-for="contact in contacts" :key="contact.name">
+                <div v-if="contact.numero == currentnumber">
+                    <p>{{ contact.name }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -28,11 +33,21 @@ export default {
         },
         call() {
 
+        },
+        namecontact(currentnumber) {
+            this.contacts.forEach(c => {
+                    if(c.numero == currentnumber) {
+                        return c.name
+                    }
+                });
         }
     },
     computed: {
         currentnumber() {
             return this.$store.state.currentnumber
+        },
+        contacts() {
+            return this.$store.state.contacts
         }
     }
     
